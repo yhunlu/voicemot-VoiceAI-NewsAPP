d25febb2d45fa6cb0b32c2ade9e970215351c729
yhunlu <yhunlu87@gmail.com>
2022-03-07 03:34:27 +0300

    initial setup react-materialui and all necessary packages regarding voice ai.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Button } from '@mui/material'
+
+function App() {
+
+  return (
+    <Button variant="contained">Hello Vite - REACT & Material-UI</Button>
+  )
+}
+
+export default App
